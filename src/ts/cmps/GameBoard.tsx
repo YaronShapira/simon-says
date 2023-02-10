3b17456a98c57d637b83c9641fd129ecdd2afc7a
ForeseN <yagosik@gmail.com>
2023-02-10 16:15:09 +0200

    added simon buttons click functionality

--- src/ts/cmps/GameBoard.tsx
+++ src/ts/cmps/GameBoard.tsx
@@ -1,7 +1,16 @@
 export default function GameBoard() {
+    function onSimonButton(ev: React.MouseEvent<HTMLDivElement>) {
+        const elTarget = ev.target as HTMLDivElement
+        // check if clicked on simon button
+        if (![...elTarget.classList].includes('simon-button')) return
+        elTarget.classList.add('clicked')
+        setTimeout(() => {
+            elTarget.classList.remove('clicked')
+        }, 1000)
+    }
     return (
         <div className='game-board'>
-            <div className='game-board-wrapper'>
+            <div className='game-board-wrapper' onClick={onSimonButton}>
                 <div className='simon-button simon-1'></div>
                 <div className='simon-button simon-2'></div>
                 <div className='simon-button simon-3'></div>
