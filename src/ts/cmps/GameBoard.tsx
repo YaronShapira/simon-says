2de8405744a654705a2937ccf8ff74b9e98960bb
ForeseN <yagosik@gmail.com>
2023-02-10 16:59:25 +0200

    added playSimonOrder func

--- src/ts/cmps/GameBoard.tsx
+++ src/ts/cmps/GameBoard.tsx
@@ -1,4 +1,13 @@
-export default function GameBoard() {
+import react, { useEffect, useRef, RefObject } from 'react'
+
+interface IProps {
+    isUserTurn: boolean
+    simonOrder: string[]
+}
+
+export default function GameBoard({ isUserTurn, simonOrder }: IProps) {
+    const gameBoardRef = useRef() as RefObject<HTMLDivElement>
+
     function onSimonButton(ev: React.MouseEvent<HTMLDivElement>) {
         const elTarget = ev.target as HTMLDivElement
         // check if clicked on simon button
@@ -8,13 +17,33 @@ export default function GameBoard() {
             elTarget.classList.remove('clicked')
         }, 1000)
     }
+
+    function wait(time: number) {
+        return new Promise((resolve, reject) => setTimeout(resolve, time))
+    }
+
+    async function playSimonOrder() {
+        for (const color of simonOrder) {
+            gameBoardRef.current?.classList.add(color)
+            await wait(750)
+            gameBoardRef.current?.classList.remove(color)
+            await wait(400)
+            console.log('WH')
+        }
+    }
+
+    useEffect(() => {
+        if (isUserTurn) return
+
+        playSimonOrder()
+    }, [isUserTurn])
     return (
-        <div className='game-board'>
+        <div className='game-board' ref={gameBoardRef}>
             <div className='game-board-wrapper' onClick={onSimonButton}>
-                <div className='simon-button simon-1'></div>
-                <div className='simon-button simon-2'></div>
-                <div className='simon-button simon-3'></div>
-                <div className='simon-button simon-4'></div>
+                <div className='simon-button green'></div>
+                <div className='simon-button red'></div>
+                <div className='simon-button yellow'></div>
+                <div className='simon-button blue'></div>
                 <div className='score'>9</div>
             </div>
         </div>
--- src/ts/pages/SimonSays.tsx
+++ src/ts/pages/SimonSays.tsx
@@ -1,9 +1,13 @@
+import React, { useState } from 'react'
 import GameBoard from '../cmps/GameBoard'
 
 export default function SimonSays() {
+    const [simonOrder, setSimonOrder] = useState(['red', 'green', 'yellow'])
+    const [isUserTurn, setIsUserTurn] = useState(false)
+
     return (
         <div className='simon-says'>
-            <GameBoard />
+            <GameBoard isUserTurn={isUserTurn} simonOrder={simonOrder} />
         </div>
     )
 }
